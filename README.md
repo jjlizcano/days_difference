83f1585f32f73429793f84e88e59abfc250d9af3
jjlizcano <julian2224647@correo.uis.edu.co>
2022-10-17 15:28:32 -0500

    Código reordenado y README extendido

---
+++ README.md
@@ -0,0 +1,19 @@
+# Díferencia de días entre dos fechas.
+
+## Funciones definidas:
+
+* `is_year_leap(year)`: Determina si un año es bisiesto.
+
+* `days_in_month(year, month)`: Regresa la cantidad de días de un mes, hace uso de la función `is_year_leap(year)`.
+
+* `days_in_year(year, month, day)`: regresa la cantidad de días desde el inicio de un año hasta un mes y un día, hace uso de la función `days_in_month(year, month)`.
+
+* `days_between_years(iyear, fyear)`: regresa la cantidad de días entre distintos años, hace uso de la función `days_in_year(year, month, day)`.
+
+Después de que se hayan recibido la fecha inicial y final, se le asigna a la variable `days_difference` la diferencia de los días entre las dos fechas, para esto, se suma la cantidad de días entre los dos años, con la cantidad de días de la fecha y se les resta la cantidad de días del año inicial.
+
+Puesto de otra manera:
+
+`days_difference = days_between_years(iyear, fyear) + (days_in_year(fyear, fmonth, fday) - days_in_year(iyear, imonth, iday))`
+
+siendo `fyear`, `fmonth` y `fday` el año, mes y día iniciales, y `iyear`, `imonth` y `iday` el año, mes y día finales.
--- días_hasta_hoy.py
+++ días_hasta_hoy.py
@@ -1,8 +1,34 @@
+# Díferencia de días entre dos fechas:
 
-print("Éste programa dice cuantos días han pasado desde una fecha hasta otra (sin incluir el último día)")
+def is_year_leap(year): # se determina si un año es bisiesto
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+def days_in_month(year, month): # se regresa la cantidad de días de un mes
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_year_leap(year):
+        days[1] = 29
+    return days[month-1]
+
+def days_in_year(year, month, day): # se regresa la cantidad de días de un año
+	days = 0
+	for m in range(1, month):
+		days += days_in_month(year, m)
+	return days + day
+
+def days_between_years(iyear, fyear): # se regresa la cantidad de días entre distintos años
+    days = 0
+    for y in range(iyear, fyear):
+        days += days_in_year(y, 12, 31)
+    return days
 
 # input
-
 print("-----Fecha inicial-----")
 iday = int(input("Día: "))
 imonth = int(input("Mes: "))
@@ -13,40 +39,8 @@ fday = int(input("Día: "))
 fmonth = int(input("Mes: "))
 fyear = int(input("Año: "))
 
-
 # processing
-
-def is_year_leap(year):
-    if year % 4 != 0:
-        return False
-    elif year % 100 != 0:
-        return True
-    elif year % 400 != 0:
-        return False
-    else:
-        return True
-
-def days_in_month(year, month):
-    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_year_leap(year):
-        days[1] = 29
-    return days[month - 1]
-
-def days_in_year(year, month, day):
-	days = 0
-	for m in range(1, month):
-		days += days_in_month(year, m)
-	return days + day
-
-def days_between_years(iyear, fyear):
-    days = 0
-    while iyear != fyear:
-        days += days_in_year(iyear, 12, 31)
-        iyear += 1
-    return days
-
-days_difference = days_between_years(iyear, fyear) + (days_in_year(fyear, fmonth, fday) - days_in_year(fyear, imonth, iday))
+days_difference = days_between_years(iyear, fyear) + (days_in_year(fyear, fmonth, fday) - days_in_year(iyear, imonth, iday))
 
 # output
-
-print(days_difference)
+print("Diferencia de días:", days_difference)
